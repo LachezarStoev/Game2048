9cdb929a1084f66df71120aa106ab7b1d0bb3369
LachezarStoev <lachezarstoev93@gmail.com>
2014-07-28 19:18:44 +0300

    smqqtai

--- src/MyMain.java
+++ src/MyMain.java
@@ -1,10 +1,9 @@
-package Main;
 import java.io.IOException;
 
 import View.ConsoleUI;
 
 
-public class Main {
+public class MyMain {
 	public static void main(String[] args) throws IOException {
 		ConsoleUI consoleUI = new ConsoleUI();
 		consoleUI.displayBoard();
